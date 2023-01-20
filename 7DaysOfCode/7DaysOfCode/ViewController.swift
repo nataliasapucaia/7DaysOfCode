56c831e733310b233d938f491b9d129130adc47c
Natália Sapucaia <natisapucaia@gmail.com>
2023-01-20 10:00:17 -0300

    first commit

---
+++ 7DaysOfCode/7DaysOfCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7DaysOfCode
+//
+//  Created by Natália Sapucaia on 20/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
